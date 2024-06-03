054aa570cb5f50ee953c787273a10415e9f29419
pedroHteles <pedroteles849@gmail.com>
2024-06-02 23:26:08 -0300

    Ajustes

--- src/context/Dados/index.jsx
+++ src/context/Dados/index.jsx
@@ -1,4 +1,5 @@
 import {createContext, useEffect, useState} from 'react';
+import { http } from '../../service/http';
 
 export const DadosContext = createContext();
 export const DadosProvider = ({children}) => {
@@ -7,6 +8,8 @@ export const DadosProvider = ({children}) => {
     const [infoLocalidade, setInfoLocalidade] = useState({})
     const [infoPlanejamento, setInfoPlanejamento] = useState({})
     const [infoInstalacao, setInfoInstalacao] = useState({})
+    const [modulos, setModulos] = useState({})
+
     const [months, setMonths] = useState({
         jan: 0,
         fev: 0,
@@ -22,68 +25,68 @@ export const DadosProvider = ({children}) => {
         dez: 0
     })
 
-    const modulos = [
-        {
-          "nome": "GENERIC 500W",
-          "valor": "500",
-          "preco": "800",
-          "area": "0.12"
-        },
-        {
-          "nome": "SOLARMAX 600W",
-          "valor": "600",
-          "preco": "1400",
-          "area": "0.5"
-        },
-        {
-          "nome": "POWERLIGHT 550W",
-          "valor": "550",
-          "preco": "1250",
-          "area": "0.5"
-        },
-        {
-          "nome": "SUNPOWER 570W",
-          "valor": "570",
-          "preco": "1350",
-          "area": "0.13"
-        },
-        {
-          "nome": "ECLIPSE 530W",
-          "valor": "530",
-          "preco": "1200",
-          "area": "0.12"
-        },
-        {
-          "nome": "SUNRISE 560W",
-          "valor": "560",
-          "preco": "1300",
-          "area": "0.14"
-        },
-        {
-          "nome": "SUPERNOVA 590W",
-          "valor": "590",
-          "preco": "1450",
-          "area": "0.16"
-        },
-        {
-          "nome": "SOLARIS 520W",
-          "valor": "520",
-          "preco": "1150",
-          "area": "0.13"
-        },
-        {
-          "nome": "SOLARFLARE 580W",
-          "valor": "580",
-          "preco": "1400",
-          "area": "0.15"
-        },
-        {
-          "nome": "SUNBLAZE 540W",
-          "valor": "540",
-          "preco": "1300",
-          "area": "0.14"
-        }
-      ]
+    // const modulos = [
+    //     {
+    //       "nome": "GENERIC 500W",
+    //       "valor": "500",
+    //       "preco": "800",
+    //       "area": "0.12"
+    //     },
+    //     {
+    //       "nome": "SOLARMAX 600W",
+    //       "valor": "600",
+    //       "preco": "1400",
+    //       "area": "0.5"
+    //     },
+    //     {
+    //       "nome": "POWERLIGHT 550W",
+    //       "valor": "550",
+    //       "preco": "1250",
+    //       "area": "0.5"
+    //     },
+    //     {
+    //       "nome": "SUNPOWER 570W",
+    //       "valor": "570",
+    //       "preco": "1350",
+    //       "area": "0.13"
+    //     },
+    //     {
+    //       "nome": "ECLIPSE 530W",
+    //       "valor": "530",
+    //       "preco": "1200",
+    //       "area": "0.12"
+    //     },
+    //     {
+    //       "nome": "SUNRISE 560W",
+    //       "valor": "560",
+    //       "preco": "1300",
+    //       "area": "0.14"
+    //     },
+    //     {
+    //       "nome": "SUPERNOVA 590W",
+    //       "valor": "590",
+    //       "preco": "1450",
+    //       "area": "0.16"
+    //     },
+    //     {
+    //       "nome": "SOLARIS 520W",
+    //       "valor": "520",
+    //       "preco": "1150",
+    //       "area": "0.13"
+    //     },
+    //     {
+    //       "nome": "SOLARFLARE 580W",
+    //       "valor": "580",
+    //       "preco": "1400",
+    //       "area": "0.15"
+    //     },
+    //     {
+    //       "nome": "SUNBLAZE 540W",
+    //       "valor": "540",
+    //       "preco": "1300",
+    //       "area": "0.14"
+    //     }
+    //   ]
       
       
     const marcas = ["Solis"]
@@ -105,6 +108,153 @@ export const DadosProvider = ({children}) => {
             xhr.send();
         }
     }, [infoLocalidade?.cep])
+
+
+    useEffect(() => {
+      buscarPaineisSolares()
+    },[])
+    const buscarPaineisSolares = async () => {
+      try {
+          console.log("CHAMOU api/paineis-solares")
+          const data = await http.get("api/paineis-solares")
+          console.log(data.request)
+          if (data.request.status === 200) {
+            console.log(data.data)
+            setModulos(data.data)
+          }else{
+            setModulos(
+            [
+                  {
+                    "nome": "GENERIC 500W",
+                    "valor": "500",
+                    "preco": "800",
+                    "area": "0.12"
+                  },
+                  {
+                    "nome": "SOLARMAX 600W",
+                    "valor": "600",
+                    "preco": "1400",
+                    "area": "0.5"
+                  },
+                  {
+                    "nome": "POWERLIGHT 550W",
+                    "valor": "550",
+                    "preco": "1250",
+                    "area": "0.5"
+                  },
+                  {
+                    "nome": "SUNPOWER 570W",
+                    "valor": "570",
+                    "preco": "1350",
+                    "area": "0.13"
+                  },
+                  {
+                    "nome": "ECLIPSE 530W",
+                    "valor": "530",
+                    "preco": "1200",
+                    "area": "0.12"
+                  },
+                  {
+                    "nome": "SUNRISE 560W",
+                    "valor": "560",
+                    "preco": "1300",
+                    "area": "0.14"
+                  },
+                  {
+                    "nome": "SUPERNOVA 590W",
+                    "valor": "590",
+                    "preco": "1450",
+                    "area": "0.16"
+                  },
+                  {
+                    "nome": "SOLARIS 520W",
+                    "valor": "520",
+                    "preco": "1150",
+                    "area": "0.13"
+                  },
+                  {
+                    "nome": "SOLARFLARE 580W",
+                    "valor": "580",
+                    "preco": "1400",
+                    "area": "0.15"
+                  },
+                  {
+                    "nome": "SUNBLAZE 540W",
+                    "valor": "540",
+                    "preco": "1300",
+                    "area": "0.14"
+                  }
+                ]
+            )
+          }
+      } catch (error) {
+        setModulos(
+          [
+                {
+                  "nome": "GENERIC 500W",
+                  "valor": "500",
+                  "preco": "800",
+                  "area": "0.12"
+                },
+                {
+                  "nome": "SOLARMAX 600W",
+                  "valor": "600",
+                  "preco": "1400",
+                  "area": "0.5"
+                },
+                {
+                  "nome": "POWERLIGHT 550W",
+                  "valor": "550",
+                  "preco": "1250",
+                  "area": "0.5"
+                },
+                {
+                  "nome": "SUNPOWER 570W",
+                  "valor": "570",
+                  "preco": "1350",
+                  "area": "0.13"
+                },
+                {
+                  "nome": "ECLIPSE 530W",
+                  "valor": "530",
+                  "preco": "1200",
+                  "area": "0.12"
+                },
+                {
+                  "nome": "SUNRISE 560W",
+                  "valor": "560",
+                  "preco": "1300",
+                  "area": "0.14"
+                },
+                {
+                  "nome": "SUPERNOVA 590W",
+                  "valor": "590",
+                  "preco": "1450",
+                  "area": "0.16"
+                },
+                {
+                  "nome": "SOLARIS 520W",
+                  "valor": "520",
+                  "preco": "1150",
+                  "area": "0.13"
+                },
+                {
+                  "nome": "SOLARFLARE 580W",
+                  "valor": "580",
+                  "preco": "1400",
+                  "area": "0.15"
+                },
+                {
+                  "nome": "SUNBLAZE 540W",
+                  "valor": "540",
+                  "preco": "1300",
+                  "area": "0.14"
+                }
+              ]
+          )
+      }
+  }
+
 
     const setInfoByKey = (key, value, set) => set(obj => { return {...obj, [key]: value} })
 
--- src/pages/Dimensionamento/index.jsx
+++ src/pages/Dimensionamento/index.jsx
@@ -8,6 +8,7 @@ import {useNavigate} from "react-router-dom";
 import {DadosContext} from "../../context/Dados";
 import CardInfo from "../../components/CardInfo";
 import colors from "../../constants/colors"
+import { http } from '../../service/http';
 
 export const Dimensionamento = () => {
     const {setLoading} = useContext(LoadingContext)
@@ -20,7 +21,7 @@ export const Dimensionamento = () => {
         estruturas,
         tensoes,
         fases,
-        setInfoPlanejamentoByKey,
+        setInfoLocalidadeByKey,
         confirmarValores,
         infoInstalacao,
         infoLocalidade 
@@ -32,11 +33,7 @@ export const Dimensionamento = () => {
     
     function calcularGeracaoDiariaPainel(horasSol,modulo,tensao) {
         // Defina a potência nominal do painel solar (em Wp)
-
-        console.log("tensao",modulo.valor)
-        console.log(modulos)
-
-        const potenciaPainel = modulo.valor; // Exemplo: 400Wp
+        const potenciaPainel = modulo.potencia; // Exemplo: 400Wp
       
         // Fator de desempenho (entre 0,75 e 0,85)
         const fatorDesempenho = 0.80;
@@ -44,8 +41,9 @@ export const Dimensionamento = () => {
         // Cálculo da geração diária por painel
         let geracaoDiariaPainel = (potenciaPainel * horasSol * fatorDesempenho) / 1000;
       
+        console.log('plha o bo',parseInt(tensao) , 100)
         // Ajuste para a tensão do sistema (110V ou 220V)
-        if (tensao === 110) {
+        if (parseInt(tensao) == 110) {
           geracaoDiariaPainel *= 0.85; // Fator de conversão para 110V (aproximado)
         }
       
@@ -67,7 +65,7 @@ export const Dimensionamento = () => {
 
     function calcularCustoTotalSistema(numeroPainéis,modulo) {
         // Custo estimado por painel solar (em reais)
-        const custoPainel = modulo.preco; // Exemplo: R$ 1.500 por painel
+        const custoPainel = modulo.valor; // Exemplo: R$ 1.500 por painel
       
         // Cálculo do custo total do sistema
         const custoTotalSistema = numeroPainéis * custoPainel;
@@ -83,7 +81,6 @@ export const Dimensionamento = () => {
     }      
 
     function calcularMelhorPainel(horas,tensao) {
-        console.log("entrou")
         let melhorIndice = -1; // Inicializa o índice do melhor painel como -1
         let melhorRelacao = 0; // Inicializa a melhor relação como 0
     
@@ -96,7 +93,7 @@ export const Dimensionamento = () => {
             const geracaoMensal = geracaoDiaria * 30; // Assumindo 30 dias no mês
             
             // Calcula a relação entre a energia gerada e o custo do módulo
-            const relacao = geracaoMensal / modulo.preco;
+            const relacao = geracaoMensal / modulo.valor;
     
             // Verifica se essa relação é melhor que a atual
             if (relacao > melhorRelacao) {
@@ -109,9 +106,7 @@ export const Dimensionamento = () => {
     }
     
 
-    React.useEffect(() => {
-        setDados( calcularConsumoSolar(infoLocalidade.media,infoLocalidade.custo ,infoLocalidade.horas,infoLocalidade.tensao))
-    },[infoLocalidade])
+
       
     function calcularConsumoSolar(mediaKwh, valorKwh, horasSol, tensao) {
         // Validação dos dados de entrada
@@ -121,9 +116,7 @@ export const Dimensionamento = () => {
 
         let indiceMelhorPainel = calcularMelhorPainel(horasSol,tensao);
 
-        console.log("indiceMelhorPainel",indiceMelhorPainel)
         const melhorModulo = modulos[indiceMelhorPainel]
-        console.log("O melhor painel solar é:",melhorModulo);
         setModulo(melhorModulo)
     
         // Cálculo da geração diária por painel
@@ -153,10 +146,63 @@ export const Dimensionamento = () => {
             custoTotalSistema: custoTotalSistema,
             areaTotal: areaTotal,
             economiaEnergiaMensal: economiaEnergiaMensal,
-            economiaFinanceiraMensal: economiaFinanceiraMensal.toFixed(2)
+            economiaFinanceiraMensal: economiaFinanceiraMensal.toFixed(2),
+            descricao:melhorModulo.descricao
         };
     }
     
+    const buscarPaineisSolares = async () => {
+        try {
+            console.log("CHAMOU api/paineis-solares/calcular")
+            console.log(infoLocalidade.modulo)
+            if(infoLocalidade.modulo){
+
+                const selectedModulo = modulos.find(modulo => modulo.descricao === infoLocalidade.modulo);
+
+                const data = await http.post("api/paineis-solares/calcular",{
+                    mediaKwh:infoLocalidade.media,
+                    valorKwh:infoLocalidade.custo,
+                    horasSol:infoLocalidade.horas,
+                    tensao: parseInt(infoLocalidade.tensao),
+                    idPainel: parseInt(selectedModulo.id)
+
+                })
+                if (data.request.status === 200) {
+                    setInfoLocalidadeByKey("modulo", data.data.descricao)
+                    console.log(data.data.descricao)
+                    console.log(data.data)
+                    setDados(data.data)
+                  }
+             
+            }else{
+                const data = await http.post("api/paineis-solares/calcular",{
+                    mediaKwh:infoLocalidade.media,
+                    valorKwh:infoLocalidade.custo,
+                    horasSol:infoLocalidade.horas,
+                    tensao: parseInt(infoLocalidade.tensao)
+                })
+                if (data.request.status === 200) {
+                    setInfoLocalidadeByKey("modulo", data.data.descricao)
+                    console.log(data.data.descricao)
+                    console.log(data.data)
+                    setDados(data.data)
+                  }
+             
+            }
+        
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    React.useEffect(() => {
+        buscarPaineisSolares()
+        //para usar sem api
+        // setDados( calcularConsumoSolar(infoLocalidade.media,infoLocalidade.custo ,infoLocalidade.horas,infoLocalidade.tensao))
+    },[infoLocalidade.modulo,infoLocalidade.tensao])
+
     // const infoLocalidade = {numeroPaineis: 2, custoTotalSistema: 2500, areaTotal: 1, economiaEnergiaMensal: 21, economiaFinanceiraMensal: "19.16"}
 
 //   const infoLocalidade = {
@@ -168,47 +214,22 @@ export const Dimensionamento = () => {
 //     tensao: "129",}
 //     console.log(infoLocalidade , "TESTE" )
       
-
+    console.log(infoLocalidade)
     return (
         <div className="dimensionamento-container">
             <div>
-                {/* <CustomInput
-                    label="Potência"
-                    prefix="kWp"
-                    value={calcularKwp(infoLocalidade?.media ,infoLocalidade?.horas, 0.8 )   }
-                    onChange={(e) => setInfoPlanejamentoByKey("potencia", e.target.value)}
-                /> */}
-
                 <CustomSelect
                     label="Módulo"
-                    options={modulos.map(modulo => modulo.nome)}
-                    value={modulo.nome}
-                    onChange={(e) => setInfoPlanejamentoByKey("modulo", e)}
+                    options={modulos.map(modulo => modulo.descricao)}
+                    value={infoLocalidade.modulo}
+                    onChange={(e) => setInfoLocalidadeByKey("modulo", e)}
                 />
-                {/* <CustomSelect
-                    label="Marca"
-                    options={marcas}
-                    value={infoPlanejamento?.marca}
-                    onChange={(e) => setInfoPlanejamentoByKey("marca", e)}
-                /> */}
-                {/* <CustomSelect
-                    label="Estrutura"
-                    options={estruturas}
-                    value={infoPlanejamento?.estrutura}
-                    onChange={(e) => setInfoPlanejamentoByKey("estrutura", e)}
-                /> */}
                 <CustomSelect
-                    label="Tensão"
-                    options={tensoes}
-                    value={infoLocalidade.tensao}
-                    onChange={(e) => setInfoPlanejamentoByKey("tensao", e)}
-                />
-                {/* <CustomSelect
-                    label="Fase"
-                    options={fases}
-                    value={infoPlanejamento?.fase}
-                    onChange={(e) => setInfoPlanejamentoByKey("fase", e)}
-                /> */}
+                        label="Tensão"
+                        options={tensoes}
+                        value={infoLocalidade?.tensao}
+                        onChange={(e) => setInfoLocalidadeByKey("tensao", e)}
+                    />
             </div>
             <Button
                 minHeight="40px"
@@ -220,30 +241,12 @@ export const Dimensionamento = () => {
             {Object.keys(infoInstalacao).length + 2 > 0 &&
                 <div>
                     <div>
-                        {/* <CustomInput
-                            label="Instalação"
-                            prefix="R$"
-                            value={infoPlanejamento?.instalacao}
-                            onChange={(e) => setInfoPlanejamentoByKey("instalacao", e.target.value)}
-                        /> */}
                         <CustomInput
                             label="Numero de painéis"
                             prefix="Unidade"
                             value={dados.numeroPaineis }
                             onChange={(e) => setInfoPlanejamentoByKey("valorMedioKit", e.target.value)}
                         />
-                        {/* <CustomInput
-                            label="Frete"
-                            prefix="R$"
-                            value={infoPlanejamento?.frete}
-                            onChange={(e) => setInfoPlanejamentoByKey("frete", e.target.value)}
-                        />
-                        <CustomInput
-                            label="Margem"
-                            prefix="R$"
-                            value={infoPlanejamento?.margem}
-                            onChange={(e) => setInfoPlanejamentoByKey("margem", e.target.value)}
-                        /> */}
                     </div>
                     <aside>
                         <Text>
